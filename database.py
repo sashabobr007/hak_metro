fbfdc1c0268ef6ca3e8dc53dd3b601b009df06b2
Alex <sashaiam123456789@yandex.ru>
2024-06-23 13:19:29 +0300

    init

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://aleksandralekseev:@localhost/metro"
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1712@localhost/metro"
+#SQLALCHEMY_DATABASE_URL = "postgresql://aleksandralekseev:@localhost/metro"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1712@localhost/metro"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
